1179ead831f09d95d15b14b3517262ce83fff4ad
Max Wang <srd073@gmail.com>
2025-09-16 17:34:35 +0800

    calc character

--- bigtext.py
+++ bigtext.py
@@ -1,9 +1,18 @@
-data=[]
-count=0
+data = []
+count = 0
 with open('reviews.txt','r') as f:
 	for line in f:
 		data.append(line)
 		count += 1
-		if (count % 1000) == 0:
+		if (count % 10000) == 0:
 			print(count)
-print(count)		
+print('檔案讀取完成,總共有',count,'筆資料！')	
+print(data[0])
+print('=====================')
+# print(data[1])
+
+dlen = 0
+for d in data:
+	dlen += len(d)
+
+print('留言的平均長度為：',dlen / len(data))	
