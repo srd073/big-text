dfbd91c91953aba7914933f4e8ed25fe69b5c74a
Max Wang <srd073@gmail.com>
2025-09-16 17:19:01 +0800

    second commit

--- bigtext.py
+++ bigtext.py
@@ -1,8 +1,9 @@
 data=[]
 count=0
-with open('reviews.txt',r) as f
-	while line in f:
+with open('reviews.txt','r') as f:
+	for line in f:
 		data.append(line)
 		count += 1
-
+		if (count % 1000) == 0:
+			print(count)
 print(count)		
