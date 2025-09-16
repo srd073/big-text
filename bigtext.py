ee08f46b14c27a1da43c2057747b5d3a046fcca0
Max Wang <srd073@gmail.com>
2025-09-16 17:48:55 +0800

    add filter function

--- bigtext.py
+++ bigtext.py
@@ -15,4 +15,19 @@ dlen = 0
 for d in data:
 	dlen += len(d)
 
-print('留言的平均長度為：',dlen / len(data))	
+print('留言的平均長度為：',dlen / len(data))	 
+
+new=[]
+for d in data:
+	if len(d)<100 :
+		new.append(d)
+
+print('一共有',len(new),'筆留言長度小於100')		
+print(new[1])
+
+
+
+
+
+
+
